093d38f29506ec645feb2a36e67fd8a8d23e3626
Shkliar Maksym <shkliar.maksym@chnu.edu.ua>
2023-04-30 23:06:34 +0300

    EX2

---
+++ OOPLab9T/EX2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::vector<int> V = { 1, 2, 2, 3, 3, 3, 4, 4, 4, 4 };
+    std::sort(V.begin(), V.end()); // сортируем вектор
+    auto it = V.begin();
+    while (it != V.end()) {
+        auto upper = std::upper_bound(it, V.end(), *it);
+        int count = std::distance(it, upper);
+        std::cout << *it << " - " << count << std::endl;
+        it = upper;
+    }
+    return 0;
+}
